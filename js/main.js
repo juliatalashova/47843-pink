03805b26a62a33214fbea346b70f8d6cdfa5f808
Julia Talashova <julia.gancheva1990@gmail.com>
2016-04-06 21:20:15 +0300

    corrected

--- js/main.js
+++ js/main.js
@@ -2,31 +2,37 @@
 'use strict';
 
 window.onload = function() {
+  var mql = window.matchMedia("screen and (max-width: 1199px)");
   var mainNav = document.getElementsByClassName('main-nav')[0];
   var mainNavToggle = document.getElementsByClassName('main-nav__toggle')[0];
   mainNavToggle.classList.remove('main-nav__toggle--opened');
   mainNavToggle.classList.add('main-nav__toggle--closed');
   mainNav.classList.remove('main-nav--opened');
   mainNavToggle.classList.add('main-nav--closed');
-  mainNav.style.display = 'none';
+  if (mql.matches) {
+    mainNav.style.display = 'none';
+  }
 
   mainNavToggle.addEventListener('click', function() {
-    if(mainNavToggle.classList.contains('main-nav__toggle--closed')) {
-      mainNavToggle.classList.add('main-nav__toggle--opened');
-      mainNavToggle.classList.remove('main-nav__toggle--closed');
-      mainNav.classList.add('main-nav--opened');
-      mainNav.classList.remove('main-nav--closed');
-      mainNav.style.display = 'block';
-      mainNav.style.position = 'absolute';
-      mainNav.style.left = 0;
-      mainNav.style.right = 0;
-      mainNav.style.top = 0;
-    } else {
-      mainNavToggle.classList.add('main-nav__toggle--closed');
-      mainNavToggle.classList.remove('main-nav__toggle--opened');
-      mainNav.classList.add('main-nav--closed');
-      mainNav.classList.remove('main-nav--opened');
-      mainNav.style.display = 'none';
+    var mql = window.matchMedia("screen and (max-width: 1199px)");
+    if (mql.matches) {
+      if (mainNavToggle.classList.contains('main-nav__toggle--closed')) {
+        mainNavToggle.classList.add('main-nav__toggle--opened');
+        mainNavToggle.classList.remove('main-nav__toggle--closed');
+        mainNav.classList.add('main-nav--opened');
+        mainNav.classList.remove('main-nav--closed');
+        mainNav.style.display = 'block';
+        mainNav.style.position = 'absolute';
+        mainNav.style.left = 0;
+        mainNav.style.right = 0;
+        mainNav.style.top = 0;
+      } else {
+        mainNavToggle.classList.add('main-nav__toggle--closed');
+        mainNavToggle.classList.remove('main-nav__toggle--opened');
+        mainNav.classList.add('main-nav--closed');
+        mainNav.classList.remove('main-nav--opened');
+        mainNav.style.display = 'none';
+      }
     }
   });
 };
