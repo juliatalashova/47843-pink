7bfd7180b419c417f6501d4286a79e595391c7b6
Julia <julia.gancheva1990@gmail.com>
2016-12-05 17:39:33 +0200

    Bugs are fixed

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 'use strict';
 
 window.onload = function() {
+  var topHeader = document.getElementsByClassName('page-header__top')[0];
   var mainNav = document.getElementsByClassName('main-nav')[0];
   var mainNavToggle = document.getElementsByClassName('main-nav__toggle')[0];
   mainNavToggle.classList.remove('main-nav__toggle--opened');
@@ -21,12 +22,27 @@ window.onload = function() {
       mainNav.style.left = 0;
       mainNav.style.right = 0;
       mainNav.style.top = 0;
+      topHeader.style.backgroundColor = '#283645';
     } else {
       mainNavToggle.classList.add('main-nav__toggle--closed');
       mainNavToggle.classList.remove('main-nav__toggle--opened');
       mainNav.classList.add('main-nav--closed');
       mainNav.classList.remove('main-nav--opened');
       mainNav.style.display = 'none';
+      topHeader.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
     }
   });
+  var mq = window.matchMedia( "(min-width: 1200px)" );
+  if (mq.matches) {
+    mainNav.classList.add('main-nav--opened');
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.style.display = 'block';
+    mainNav.style.position = 'absolute';
+    mainNav.style.right = 129;
+    mainNav.style.top = 35;
+  } else {
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
+    mainNav.style.display = 'none';
+  }
 };
